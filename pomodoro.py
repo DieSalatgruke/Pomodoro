798b3949bda3320b1e9070375cd320bad88f080c
DieSalatgurke <marco.holzeapfel.protonmail.com>
2022-04-11 17:55:22 +0200

    Pomordorotimer fertig mit Sound und mit For-Schleife

--- pomodoro.py
+++ pomodoro.py
@@ -2,6 +2,7 @@ import time
 import json
 import sys
 import tkinter as tk
+import winsound
 
 CONFIG_DATA = 'config_pomo.json'
 
@@ -31,14 +32,14 @@ def get_timer_config():
         return config_data
 
 
-def request(inp):
-    if inp == 'easy':
+def request(_inp):
+    if _inp == 'easy':
         return get_timer_config()['intervall'][0]
-    elif inp == 'normal':
+    elif _inp == 'normal':
         return get_timer_config()['intervall'][1]
-    elif inp == 'hard':
+    elif _inp == 'hard':
         return get_timer_config()['intervall'][2]
-    elif inp == 'exit' or inp == 'quit':
+    elif _inp == 'exit' or _inp == 'quit':
         sys.exit()
     else:
         print('Falsche Eingabe!')
@@ -54,39 +55,51 @@ def countdown(num_of_secs):
         time.sleep(1)
 
 
-def timer_to_pomodoro(interval_intensity):
-    while interval_intensity > 1:
-        make_it_beauty()
-        print('Aktivezeit')
-        make_it_beauty()
-        countdown(_aktiv())
+def calculate_big_break(_interval):
+    _big_breaks = _interval/3
+    if str(_big_breaks)[-1] == '0':
+        _big_breaks -= 1
+        return int(_big_breaks)
+    else:
+        _big_breaks = _interval // 3
+        return _big_breaks
 
-        make_it_beauty()
-        print('Pause')
-        make_it_beauty()
-        countdown(_break())
 
-        interval_intensity -= 1
+def timer_for_big_break():
+    sound()
+    make_it_beauty()
+    print('Große Pause')
+    make_it_beauty()
+    countdown(_big_break())
 
-        if interval_intensity == 3 or interval_intensity == 6 or interval_intensity == 9 or interval_intensity == 12:
-            make_it_beauty()
-            print('Aktivezeit, gleich kommt die große Pause!')
-            make_it_beauty()
-            countdown(_aktiv())
 
-            make_it_beauty()
-            print('Große Pause')
-            make_it_beauty()
-            countdown(_big_break())
+def timer_for_break():
+    sound()
+    make_it_beauty()
+    print('Pause')
+    make_it_beauty()
+    countdown(_break())
 
-            interval_intensity -= 1
 
-        elif interval_intensity == 1:
-            make_it_beauty()
-            print('Letzte Aktivezeit')
-            make_it_beauty()
-            countdown(_aktiv())
-            finished()
+def timer_for_aktiv():
+    sound()
+    make_it_beauty()
+    print('Aktivezeit')
+    make_it_beauty()
+    countdown(_aktiv())
+
+
+def create_list_big_break(interval_intensity):
+    break_list = [2]
+    start_break = 2
+    for elm in range(calculate_big_break(interval_intensity)):
+        start_break = start_break + 3
+        break_list.append(start_break)
+    return break_list
+
+
+def sound():
+    winsound.PlaySound('*', winsound.SND_ALIAS)
 
 
 def make_it_beauty():
@@ -117,9 +130,19 @@ if __name__ == '__main__':
                         f"hard = ({get_timer_config()['intervall'][2]})?: "))
 
         inpt = request(inp)
+
         if inpt is False:
             i = 1
         else:
-            timer_to_pomodoro(inpt)
-            i = 2
-
+            b_list = create_list_big_break(inpt)
+            for t in range(inpt):
+                if t in b_list:
+                    timer_for_aktiv()
+                    timer_for_big_break()
+                elif t == inpt-1:
+                    timer_for_aktiv()
+                    finished()
+                else:
+                    timer_for_aktiv()
+                    timer_for_break()
+        i = 2
